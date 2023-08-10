dd2e2efb55d447e0e2287b3c2634d2c10af1e398
A3S4ROBOY <akmalovasror0@gmail.com>
2023-08-10 17:29:00 +0300

    I added Repository

--- postgress/AppDbContext/AppDbContext.cs
+++ postgress/AppDbContext/AppDbContext.cs
@@ -7,7 +7,7 @@ namespace postgress.AppDbContext;
 public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){ }
-    public DbSet<User> Users { get; set; }
+    public DbSet<User?> Users { get; set; }
     public DbSet<Comment> Comments { get; set; }
     public DbSet<Contact> Contacts { get; set; }
     public DbSet<Course> Courses { get; set; }
---
+++ postgress/DTO's/UsersDto.cs
@@ -0,0 +1,8 @@
+﻿namespace postgress.DTO_s;
+
+public class UsersDto
+{
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+    public string? Password { get; set; }
+}
