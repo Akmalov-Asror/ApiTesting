f0886528ab0d06ec51a326cfa459717400c1b0d9
A3S4ROBOY <akmalovasror0@gmail.com>
2023-08-21 10:43:36 +0300

    new items

---
+++ postgress/Validators/DescriptionCourseValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using postgress.Entities;
+
+namespace postgress.Validators;
+
+public class DescriptionCourseValidator : AbstractValidator<DescriptionCourse>
+{
+    public DescriptionCourseValidator()
+    {
+        RuleFor(description => description.Title).NotEmpty().WithMessage("Title is required.");
+        RuleFor(description => description.Review).NotEmpty().WithMessage("Review is required.");
+        RuleFor(descriptionCourse => descriptionCourse.CourseId).NotEmpty().WithMessage("CourseId is required.")
+            .Must(BeAGuid).WithMessage("CourseId must be a valid Guid.");
+        
+    }
+
+    private bool BeAGuid(Guid? guid) => guid != null && guid != Guid.Empty;
+}
